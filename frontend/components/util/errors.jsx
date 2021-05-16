0ffd47d7d689c9ef131378c23f542cb834a38b7b
fdiengott <freddiengott@gmail.com>
2021-05-16 18:53:33 -0400

    make a standalone errors component

---
+++ frontend/components/util/errors.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+const Errors = ({ errors }) => {
+  if (!!errors.length) return null; 
+
+  return (
+    <ul className="form-errors">
+      {errors.map((err, i) => ( <li key={i}>{err}</li> ))}
+    </ul>
+  )
+}
+
+export default Errors; 
