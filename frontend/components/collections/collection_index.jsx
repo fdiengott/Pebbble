762f60bcd14a0e6ab7ce5944ca7e56ea87f6fa18
fdiengott <freddiengott@gmail.com>
2021-05-20 00:25:29 -0400

    add and style a no collections component

--- frontend/components/collections/collection_index.jsx
+++ frontend/components/collections/collection_index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'; 
 
 import CollectionIndexItem from './collection_index_item'; 
+import NoCollections from './no_collections'; 
 
 class CollectionIndex extends React.Component {
   constructor(props) {
     super(props); 
+    this.state = { noCollections: false }
   }
 
   componentDidMount() {
-    this.props.fetchUserCollections(this.props.userId); 
+    this.props.fetchUserCollections(this.props.userId)
+      .then( null, () => this.setState({ noCollections: true })); 
   }
 
   render () {
@@ -21,6 +24,9 @@ class CollectionIndex extends React.Component {
     const collectionsArr = Object.values(collections); 
     const cardsArr = Object.values(cards); 
 
+    if (this.state.noCollections) {
+      return <NoCollections />
+    }
 
     return (
       <ul className="collections-index-list" role="list">
