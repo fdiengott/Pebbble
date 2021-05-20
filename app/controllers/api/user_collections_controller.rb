adc4b382030c1b63ce0b9046304417380d09b5fa
fdiengott <freddiengott@gmail.com>
2021-05-20 00:12:55 -0400

    bugfix user collections controller if there are no collections found

--- app/controllers/api/user_collections_controller.rb
+++ app/controllers/api/user_collections_controller.rb
@@ -2,7 +2,13 @@ class Api::UserCollectionsController < ApplicationController
 
   def index
     @collections = User.find_by(id: params[:curator_id]).collections.includes(:cards)
-    render '/api/collections/index'
+
+    if @collections.count > 0
+      render '/api/collections/index'
+    else 
+      render json: ["No collections found"], status: 404
+    end
+
   end
 
 end
