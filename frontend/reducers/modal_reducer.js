46ac4b24aaa9484f0863501e230264e86f851186
fdiengott <freddiengott@gmail.com>
2021-05-20 16:52:37 -0400

    add modal reducer and update ui reducer

---
+++ frontend/reducers/modal_reducer.js
@@ -0,0 +1,19 @@
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions'; 
+
+
+const modalReducer = (state = false, action) => {
+  Object.freeze(state); 
+
+  switch (action.type) {
+    case OPEN_MODAL:
+      return true; 
+    case CLOSE_MODAL:
+      return false; 
+      
+    default:
+      return state;
+  }
+}
+  
+
+export default modalReducer; 
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,19 +1,8 @@
-import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions'; 
+import { combineReducers } from 'redux'; 
+import modal from './modal_reducer'; 
 
-
-const uiReducer = (state = {}, action) => {
-  Object.freeze(state); 
-
-  switch (action.type) {
-    case OPEN_MODAL:
-      return true; 
-    case CLOSE_MODAL:
-      return false; 
-      
-    default:
-      return state;
-  }
-}
-  
+const uiReducer = combineReducers({
+  modal,
+});
 
 export default uiReducer; 
