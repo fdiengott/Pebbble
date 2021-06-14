68deb2e0af8216bfb869b3221ff39985081e8a7b
fdiengott <freddiengott@gmail.com>
2021-06-14 16:40:04 -0400

    Add likes and user likes controllers

---
+++ app/controllers/api/user_likes_controller.rb
@@ -0,0 +1,12 @@
+class Api::UserLikesController < ApplicationController 
+
+  def index
+    @likes = Like.where(liker_id: params[:user_id])
+    if @likes
+      render 'api/likes/index'
+    else
+      render json: ["User not found"], status: 404
+    end
+  end
+
+end
